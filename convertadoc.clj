c1e228c74bc7cf6496eaf5898f372a27d7bccdbf
George Kontsevich <geokon@qq.com>
2024-03-02 21:21:02 +0800

    Edit: cleanup

--- convertadoc.clj
+++ convertadoc.clj
@@ -1,6 +1,5 @@
 (ns convertadoc
   "Simple functions to convert Asciidoc `.adoc` files to HTML or whatever"
-;;  (:use [clojure.tools.deps.alpha.repl])
   (:import [org.asciidoctor
             Asciidoctor
             OptionsBuilder
@@ -9,34 +8,25 @@
 (defn
   to-html
   [filestr] ;; "/home/kxygk/Projects/stars/Zeeden2023.adoc"
-  (let [input-file (clojure.java.io/file filestr)
-        adoctor    (org.asciidoctor.Asciidoctor$Factory/create)
+  (let [input-file    (clojure.java.io/file filestr)
+        adoctor       (org.asciidoctor.Asciidoctor$Factory/create)
         reveal-option (doto (org.asciidoctor.OptionsBuilder/options)
-                        (.backend "html5")
-                        #_#_
-                        (.safe org.asciidoctor.SafeMode/UNSAFE)
-                        #(.attributes (.attribute (org.asciidoctor.AttributesBuilder/attributes)
-                                                 "revealjsdir"
-                                                 "../reveal.js")))]
-    #_
-    (.requireLibrary adoctor
-                     (into-array String
-                                 ["asciidoctor-revealjs"]))
+                        (.backend "html5"))]
     (.convertFile adoctor
                   input-file
                   reveal-option)))
 
 (defn
   to-reveal
-  [filestr] ;; "/home/kxygk/Projects/stars/Zeeden2023.adoc"
-  (let [input-file (clojure.java.io/file filestr)
-        adoctor    (org.asciidoctor.Asciidoctor$Factory/create)
+  [filestr]
+  (let [input-file    (clojure.java.io/file filestr)
+        adoctor       (org.asciidoctor.Asciidoctor$Factory/create)
         reveal-option (doto (org.asciidoctor.OptionsBuilder/options)
                         (.backend "revealjs")
                         (.safe org.asciidoctor.SafeMode/UNSAFE)
                         #(.attributes (.attribute (org.asciidoctor.AttributesBuilder/attributes)
-                                                 "revealjsdir"
-                                                 "../reveal.js")))]
+                                                  "revealjsdir"
+                                                  "../reveal.js")))]
     (.requireLibrary adoctor
                      (into-array String
                                  ["asciidoctor-revealjs"]))
